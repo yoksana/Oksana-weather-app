638865cc493c1b9a10d4d48d15240c7143aba509
yoksana <oksana.bil3@gmail.com>
2022-08-25 19:01:38 +0300

    Added dynamic 5day fcst

--- src/index.js
+++ src/index.js
@@ -21,29 +21,49 @@ function checkTime(time) {
   }
   return `${dayweek} ${hour}:${minute}`;
 }
-function weekForecast() {
+
+function formatDay(timestamp) {
+  let date = new Date(timestamp * 1000);
+  console.log(date);
+  let daynum = date.getDay();
+  let names = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sut"];
+  let dateformat = names[daynum];
+  return dateformat;
+}
+function weekForecast(weekday) {
+  let data = weekday.data.daily;
+  console.log(data);
   let forecast = document.querySelector("#weekfcst");
-  let days = ["Mon", "Tue", "Wed", "Thu", "Fri"];
+
   let forecastHTML = "";
-  days.forEach(function (day) {
-    forecastHTML =
-      forecastHTML +
-      `
+  data.forEach(function (day, index) {
+    if (index < 5) {
+      forecastHTML =
+        forecastHTML +
+        `
             <div class="col">
-              <div class = "fcst-date">${day}</div>
-              <img src="http://openweathermap.org/img/wn/10d@2x.png" width="70px"  alt=""/>        
+              <div class = "fcst-date">${formatDay(day.dt)}</div>
+              <img src="http://openweathermap.org/img/wn/${
+                day.weather[0].icon
+              }@2x.png" width="70px"  alt=""/>        
               <div class="fcst-temp">
-                <span class="fcst-temp-max"> 28° </span>
-                <span class="fcst-temp-min"> 20° </span>
+                <span class="fcst-temp-max">${Math.round(day.temp.max)}°</span>
+                <span class="fcst-temp-min">${Math.round(day.temp.min)}°</span>
               </div>
             </div>
   `;
+    }
   });
   forecast.innerHTML = forecastHTML;
 }
 
+function getforecast(coordinates) {
+  let apiKey = "89b5d6145e65f229b79e3ab3372a1a19";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(weekForecast);
+}
+
 function showTemp(response) {
-  console.log(response.data);
   let temperature = document.querySelector("#temp");
   let city = document.querySelector("#mycity");
   let humidity = document.querySelector("#humidity");
@@ -62,6 +82,7 @@ function showTemp(response) {
     `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
   );
   time.innerHTML = checkTime(response.data.dt * 1000);
+  getforecast(response.data.coord);
 }
 
 function searchCity(city) {
@@ -93,7 +114,6 @@ let selectButton = document.querySelector("#search-form");
 selectButton.addEventListener("submit", cityChange);
 
 searchCity("Kyiv");
-weekForecast();
 
 let selectFar = document.querySelector("#fahrenheit");
 selectFar.addEventListener("click", farChange);
